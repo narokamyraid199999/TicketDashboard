971de59424d9cf525b7b433d84abbe15bf9dfa47
narokamyraid199999 <143643793+narokamyraid199999@users.noreply.github.com>
2024-09-10 06:19:29 -0700

    add simple chart

--- src/app/Components/dashboard/Components/chart/chart.component.ts
+++ src/app/Components/dashboard/Components/chart/chart.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.css'],
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    this.chartInit();
+  }
+
+  chartInit() {
+    type EChartsOption = echarts.EChartsOption;
+    var chartDom = document.getElementById('main')!;
+    var myChart = echarts.init(chartDom);
+    var option: EChartsOption;
+
+    option = {
+      color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
+      title: {
+        text: 'Gradient Stacked Area Chart',
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985',
+          },
+        },
+      },
+      legend: {
+        data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5'],
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+        },
+      ],
+      series: [
+        {
+          name: 'Line 1',
+          type: 'line',
+          stack: 'Total',
+          smooth: true,
+          lineStyle: {
+            width: 0,
+          },
+          showSymbol: false,
+          areaStyle: {
+            opacity: 0.8,
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: 'rgb(128, 255, 165)',
+              },
+              {
+                offset: 1,
+                color: 'rgb(1, 191, 236)',
+              },
+            ]),
+          },
+          emphasis: {
+            focus: 'series',
+          },
+          data: [140, 232, 101, 264, 90, 340, 250],
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,18 @@ import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { ChartComponent } from './Components/dashboard/Components/chart/chart.component';
 import { CardComponent } from './Components/dashboard/Components/card/card.component';
 import { TableComponent } from './Components/dashboard/Components/table/table.component';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, ChartComponent, CardComponent, TableComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ChartComponent,
+    CardComponent,
+    TableComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, CardModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
