c7de99a9cc91a739c16d8b8584ab94b925816225
narokamyraid199999 <143643793+narokamyraid199999@users.noreply.github.com>
2024-09-10 05:07:32 -0700

    install packages and make the general layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { ChartComponent } from './Components/dashboard/Components/chart/chart.component';
+import { CardComponent } from './Components/dashboard/Components/card/card.component';
+import { TableComponent } from './Components/dashboard/Components/table/table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DashboardComponent, ChartComponent, CardComponent, TableComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
